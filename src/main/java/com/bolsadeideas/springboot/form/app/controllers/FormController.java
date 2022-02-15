722b330049aa56e36b88a3b6808c7a5797170106
Carlos Barrios Pereira <caebarri@bancolombia.com.co>
2022-02-15 01:00:09 -0500

    Adding User class

--- src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -1,5 +1,6 @@
 package com.bolsadeideas.springboot.form.app.controllers;
 
+import com.bolsadeideas.springboot.form.app.models.domain.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,15 +21,17 @@ public class FormController {
     }
 
     //Procesar los datos enviados por el usuario
-    //@RequestParam para capturar los valores del fomrulario con name en sus etiquetas
+    //@RequestParam para capturar los valores del formulario con name en sus etiquetas
     @PostMapping("/form")
     public String processForm(
             Model model, @RequestParam String username, @RequestParam String password, @RequestParam String email) {
 
+        //Al ser una clase con datos (que se deben persistir, o eliminar, o mostrar, etc), y no con un servicio
+        //que "prestar", esta no se inyecta
+        User newUser = new User(username, password, email);
+
         model.addAttribute("title", "Filled form");
-        model.addAttribute("username", username);
-        model.addAttribute("password", password);
-        model.addAttribute("email", email);
+        model.addAttribute("newUser", newUser);
         return "filled";
     }
 
