ad66b6d4070d12f55828616c931dad6b26d7687d
Carlos Barrios Pereira <caebarri@bancolombia.com.co>
2022-03-02 17:42:02 -0500

    Adding id, name and lastname fields

--- src/main/java/com/bolsadeideas/springboot/form/app/models/domain/User.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/models/domain/User.java
@@ -1,14 +1,21 @@
 package com.bolsadeideas.springboot.form.app.models.domain;
 
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 
 public class User {
+
+    private String id;
 
     //Los atributos deben ser iguales a los name en el form que va a recibir la información
     //Las validaciones que se activaron en el controller con @Valid se especifican acá
     @NotEmpty //Distinto de null y no vacio
     private String username;
+
+    @NotEmpty
+    private String name;
+
+    @NotEmpty
+    private String lastname;
 
     @NotEmpty
     private String password;
@@ -19,10 +26,12 @@ public class User {
     public User() {
     }
 
-    public User(String username, String password, String email) {
-        this.username = username;
-        this.password = password;
-        this.email = email;
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getUsername() {
@@ -47,5 +56,21 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
     }
 }
